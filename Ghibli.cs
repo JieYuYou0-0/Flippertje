1da20eaaaa3f7ef57e6477ac65520973cb92b691
Jie Yu You <jy_you@hotmail.com>
2022-05-30 12:47:26 +0200

    A new ghibli will be made when a program is running.

---
+++ Ghibli.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GhibliFlix
+{
+    internal class Ghibli
+    {
+        public void Run()
+        {
+            Menu.ClearLog();
+            //LanguageMenu languageMenu = new LanguageMenu();
+            //languageMenu.Init();
+        }
+
+    }
+}
