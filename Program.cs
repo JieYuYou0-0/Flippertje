247365a0e6a0a42c17e07195c271704cff7baa8a
Jie Yu You <jy_you@hotmail.com>
2022-05-30 13:30:48 +0200

    .

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace GhibliFlix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+    }
+}
