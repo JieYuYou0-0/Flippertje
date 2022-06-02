6424389e8c498b1902bef68303a618c3b47b0fb6
Melisa Çiftçi <1038404@hr.nl>
2022-06-02 18:52:26 +0200

    First step of the welcome screen

---
+++ WelcomeScreen.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GhibliFlix;
+
+namespace GhibliFlix
+{
+    internal class WelcomeScreen
+    {
+        private readonly Screen next;
+
+        public WelcomeScreen_1()
+        {
+            next = new Menu_2();
+            next.SetPrevious(this);
+        }
+        public override void Show()
+        {
+            Log("[Step 1]");
+
+            ConsoleKeyInfo input;
+
+            do
+            {
+                Console.Clear();
+
+                Display("Welcome, dear guest.");
+                Display("Please press [ENTER] to continue...");
+
+                input = Console.ReadKey();
+            }
+            while (input.Key != ConsoleKey.Enter);
+
+            next.Show();
+        }
+    }
+}
