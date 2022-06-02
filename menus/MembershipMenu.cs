f66dd47e397cf755abb92821b7e4f4368f23e01e
Jie Yu You <jy_you@hotmail.com>
2022-06-02 21:26:15 +0200

    .

---
+++ menus/MembershipMenu.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GhibliFlix.menus
+{
+    internal class MembershipMenu : Menu
+    {
+        internal MembershipMenu()
+        {
+            CreateReservationMenu createReservationMenu = new CreateReservationMenu();
+            //createReservationMenu
+
+        }
+    }
+}
