713b313662cff9e7489fbb162d328f981658ef15
Jie Yu You <jy_you@hotmail.com>
2022-06-07 23:06:24 +0200

    movie overview in json

--- MovieOverview.cs
+++ MovieOverview.cs
@@ -1,66 +1,66 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Runtime.InteropServices.ComTypes;
-//using System.Text;
-//using System.Threading.Tasks;
-//using Microsoft.VisualBasic.CompilerServices;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic.CompilerServices;
 
 
-//namespace GhibliFlix
-//{
-//    internal class MovieOverview 
-//    {
-//        private string title;
-//        private double price;
+namespace GhibliFlix
+{
+    internal class MovieOverview : Menu
+    {
+        private string title;
+        private double price;
 
-//        internal MovieOverview()
-//        {
+        internal MovieOverview()
+        {
 
-//        }
+        }
 
-//        internal string[] GetTitles()
-//        {
-//            string[] titles = new string[]
-//            {
-//                "My Neighbor Totoro",
-//                "Howl's Moving Castle",
-//                "Princess Mononoke",
-//                "Kiki's Delivery Service",
-//                "Ponyo",
-//                "Spirited Away",
-//                "The Cat Returns",
-//                "The Wind Rises"
-//            };
-//            foreach (string title in titles)
-//            {
-//                Random rnd = new Random();
-//                rnd.Next(titles.Length);
-//            }
+        internal string[] GetTitles()
+        {
+            string[] titles = new string[]
+            {
+                "My Neighbor Totoro",
+                "Howl's Moving Castle",
+                "Princess Mononoke",
+                "Kiki's Delivery Service",
+                "Ponyo",
+                "Spirited Away",
+                "The Cat Returns",
+                "The Wind Rises"
+            };
+            foreach (string title in titles)
+            {
+                Random rnd = new Random();
+                rnd.Next(titles.Length);
+            }
 
-//            return titles;
-//        }
+            return titles;
+        }
 
-//        internal double[] GetPrices()
-//        {
-//            double[] prices = new double[8];
-//            for (double i = 0.00; i < prices.Length; i++)
-//            {
-//                Random rnd = new Random();
-//                rnd.Next(10, 20);
-//            }
+        internal double[] GetPrices()
+        {
+            double[] prices = new double[8];
+            for (double i = 0.00; i < prices.Length; i++)
+            {
+                Random rnd = new Random();
+                rnd.Next(10, 20);
+            }
 
-//            return prices;
-//        }
-//        internal Tuple<string, double>[] GetMovieCollection()
-//        {
-//            Tuple<string, double>[] movieCollection = new Tuple<string, double>[8];
-//            for (int i = 0; i < movieCollection.Length; i++)
-//            {
-//                Tuple.Create(title, price);
-//            }
-//            return movieCollection;
-//        }
-//    }
-//}
+            return prices;
+        }
+        internal Tuple<string, double>[] GetMovieCollection()
+        {
+            Tuple<string, double>[] movieCollection = new Tuple<string, double>[8];
+            for (int i = 0; i < movieCollection.Length; i++)
+            {
+                Tuple.Create(title, price);
+            }
+            return movieCollection;
+        }
+    }
+}
 
