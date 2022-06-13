b455dd120ff7f22cd99aa77de0a390f640331037
Melisa Çiftçi <1038404@hr.nl>
2022-06-13 11:30:56 +0200

    .

--- SignUp.cs
+++ SignUp.cs
@@ -4,9 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Text.Json;
+using GhibliFlix.json_class;
 
-namespace GhibliFlix 
-{ 
+namespace GhibliFlix
+{
+    internal class SignUp
+    {
+        static SignUp reg = new SignUp();
+
+        internal static void SignUpUser(Members.CustomerDetails data)
+        {
+            //Read existing JSON data
+            var jsonData = File.ReadAllText(@"json_files\members.json");
 
             //Deserialize data and if not exist, create new list
             List<Members.CustomerDetails> jsonList;
@@ -23,16 +32,19 @@ namespace GhibliFlix
             jsonList.Add(data);
 
             jsonData = JsonSerializer.Serialize(jsonList);
+            File.WriteAllText(@"json_files\members.json", jsonData);
+        }
 
-        
+        internal static bool VerifyUser(string passwordCheck)
+        {
             Console.WriteLine("Type STOP to cancel verifying your account");
             string input = Console.ReadLine();
             if (input == "STOP")
             {
                 return false;
             }
-
-            
+            while (input != passwordCheck)
+            {
                 Console.WriteLine("It looks like your given code is not valid. Please try again!");
                 input = Console.ReadLine();
                 if (input == "STOP")
@@ -43,8 +55,8 @@ namespace GhibliFlix
             return true;
         }
         //Checking credit card number
-
-        
+        internal static bool IsDigitsOnly(string str)
+        {
             foreach (char c in str)
             {
                 if (c < '0' || c > '9')
@@ -54,7 +66,6 @@ namespace GhibliFlix
             }
             return true;
         }
-    }
     }
 }
 
