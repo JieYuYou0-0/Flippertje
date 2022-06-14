9e4273357d1a3ed8e89d100e717622396e648ae0
Jie Yu You <jy_you@hotmail.com>
2022-06-14 20:46:03 +0200

    Create a menu for settings.

---
+++ menus/SettingsMenu.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using GhibliFlix.jsonClasses;
+using GhibliFlix.menus;
+
+namespace GhibliFlix
+{
+    internal class SettingsMenu : Menu
+    {
+        internal MenuOverview menuOverview;
+        readonly Settings settings;
+
+        internal SettingsMenu()
+        {
+            string settingsJson = File.ReadAllText("json_files/settings.json");
+            settings = JsonSerializer.Deserialize<Settings>(settingsJson);
+        }
+    }
+}
