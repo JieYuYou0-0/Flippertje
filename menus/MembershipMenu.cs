72dfb987839f5008f63f480656158b2707a57552
Jie Yu You <jy_you@hotmail.com>
2022-07-06 04:45:57 +0200

    Update MembershipMenu.cs

--- menus/MembershipMenu.cs
+++ menus/MembershipMenu.cs
@@ -1,75 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
-using System.Threading.Tasks;
-using GhibliFlix.menus;
+using System.Net;
+using System.Net.Mail;
 
 namespace GhibliFlix
 {
-    public class MembershipMenu : Menu
+    class MembershipMenu : Menu
     {
         public MembershipMenu()
         {
             CreateReservationMenu createReservationMenu = new CreateReservationMenu();
             createReservationMenu.PreviousMenu = Init;
 
-            AddMenuOption(MembershipOverview, ConsoleKey.D1, Session.Settings.MembershipOverview);
-            AddMenuOption(createReservationMenu.Init, ConsoleKey.D2, Session.Settings.MembershipCreateReservation);
-            AddMenuOption(CancelReservation, ConsoleKey.D3, Session.Settings.MembershipCancelReservation);
-
+            AddMenuOption(MembershipOverview, ConsoleKey.D1, Session.Language.MembershipOverview);
+            AddMenuOption(createReservationMenu.Init, ConsoleKey.D2, Session.Language.MembershipCreateReservation);
+            AddMenuOption(CancelReservation, ConsoleKey.D3, Session.Language.MembershipCancelReservation);
         }
 
         public override void Init()
         {
             Console.Clear();
             Menu.Log("Opens Membership Menu");
-            OverviewMenu menuOverview = new OverviewMenu();
-            PreviousMenu = menuOverview.Init;
+            GuestMenu guestmenu = new GuestMenu();
+            PreviousMenu = guestmenu.Init;
 
-            string greet = Session.Settings.MembershipMenuGreet;
+            string greet = Session.Language.MembershipMenuGreet;
             Console.WriteLine(string.Format(greet, Session.User.Name));
-            Console.WriteLine(Session.Settings.MembershipMenuTitle);
+            Console.WriteLine(Session.Language.MembershipMenuTitle);
             ShowMenu();
         }
 
-        public void MembershipOverview()
-        {
-
-        }
-
+        #region Cancel Reservation
         public void CancelReservation()
         {
+
             Log("Cancel Reservation Menu");
 
             PreviousStep = Init;
             Console.Clear();
 
             int indexReservation = 0;
-            bool bcontinue = false;
+            bool bDoorgaan = false;
 
-            Console.WriteLine(Session.Settings.CancelReservation);
-            Console.WriteLine(Session.Settings.AskVerificationCode);
+            Console.WriteLine(Session.Language.CancelReservation);
+            Console.WriteLine(Session.Language.AskVerificationCode);
 
             string VerificatieCode = ReadLine();
-            string json = File.ReadAllText("json_files/reservations.json");
+            //var OpenReservation = Reservations.FetchAll();
+
+            string json = File.ReadAllText("jsonFiles/reservations.json");
             var Reservations = JsonSerializer.Deserialize<Reservations>(json);
 
             int ReservationsAmount = Reservations.reservations.Count;
             for (int i = 0; i < ReservationsAmount; i++)
             {
+                //var open = Reservations.reservations[i];
                 if (Reservations.reservations[i].ReservationId == VerificatieCode)
                 {
                     indexReservation = i;
-                    bcontinue = true;
+                    bDoorgaan = true;
                 }
             }
-
-            if (bcontinue == false)
+            if (bDoorgaan == false)
             {
                 Menu.Log("Input invalid for cancel reservation");
-                Console.WriteLine(Session.Settings.InvalidInput);
+                Console.WriteLine(Session.Language.InvalidInput);
                 PreviousStep = CancelReservation;
                 ReadBackInput();
             }
@@ -81,59 +81,104 @@ namespace GhibliFlix
 
             if (differenceInDates < 7)
             {
-                bcontinue = false;
-                Console.WriteLine(Session.Settings.CancelTooLate);
+                bDoorgaan = false;
+                Console.WriteLine(Session.Language.CancelTooLate);
                 PreviousStep = Init;
                 ReadBackInput();
 
             }
-
-            if (bcontinue == true)
+            if (bDoorgaan == true)
             {
-                Console.WriteLine(Session.Settings.ReservationCancelOptions);
+                Console.WriteLine(Session.Language.ReservationCancelOptions);
                 string keuze = ReadLine();
                 if (keuze == "1")
                 {
-                    // Completely deleted
-                    Menu.Log("Howl removes everything from reservation");
+                    //Volledig verwijderen
+                    Menu.Log("Ghiblix removes whole reservation");
                     PreviousStep = Init;
 
                     Reservations.reservations[indexReservation].Cancelled = true;
                     string Newjson = JsonSerializer.Serialize(Reservations);
 
-                    File.WriteAllText("json_files/reservations.json", Newjson);
-                    Console.WriteLine(Session.Settings.ReservationCanceled);
+                    File.WriteAllText("jsonFiles/reservations.json", Newjson);
+                    Console.WriteLine(Session.Language.ReservationCanceled);
                 }
                 else if (keuze == "2")
                 {
-                    Menu.Log("Howl removes guests from reservation");
+                    Menu.Log("Ghiblix removes guests from reservation");
                     PreviousStep = Init;
 
-                    // Partly deleted
-                    Console.WriteLine(Session.Settings.NamesToRemove);
+                    //Deels verwijderen
+                    //Console.WriteLine(Session.Language.NamesToRemove);
                     string namesInput = Console.ReadLine();
                     char[] delimiterChars = { ' ', ',', '.' };
                     string[] words = namesInput.Split(delimiterChars);
+                    var Guests = Reservations.reservations[indexReservation].Guests;
+                    for (int i = 0; i < Guests.Count; i++)
+                    {
+                        var GuestName = Guests[i].Name;
+                        foreach (var word in words)
+                        {
+                            if (GuestName.ToLower() == word.ToLower())
+                            {
+                                Guests.RemoveAt(i);
+                            }
+                        }
+                    }
 
                     string json2 = JsonSerializer.Serialize(Reservations);
-                    File.WriteAllText("json_files/reservations.json", json2);
+                    File.WriteAllText("jsonFiles/reservations.json", json2);
 
-                    Console.WriteLine(Session.Settings.ReservationChanged);
+                    Console.WriteLine(Session.Language.ReservationChanged);
                 }
                 else
                 {
-                    Console.WriteLine(Session.Settings.InvalidInput);
+                    Console.WriteLine(Session.Language.InvalidInput);
                     PreviousStep = CancelReservation;
                     ReadBackInput();
                 }
 
-                string htmlBody = File.ReadAllText(@"emailTemplates/reservation_cancelConfirmation.txt");
+                string htmlBody = htmlBody = File.ReadAllText(@"emailTemplates/reservationCancelConfirmation.txt");
                 List<string> email = new List<string> { Session.User.Email };
 
-                MailSender.SendConfirmationMail(htmlBody, email, Session.Settings.EmailCancelRes);
+                MailSender.SendConfirmationEmail(htmlBody, email, Session.Language.EmailCancelReservation);
                 ReadBackInput();
-
             }
         }
+
+        #endregion Cancel Reservation
+
+        #region Membership Overview
+        public void MembershipOverview()
+        {
+            Menu.Log("Ghiblix opens MembershipOverview");
+            PreviousStep = Init;
+
+            string settingsJson = File.ReadAllText("jsonFiles/memberships.json");
+            Members memberList = JsonSerializer.Deserialize<Members>(settingsJson);
+            Member member = memberList.members.FirstOrDefault(x => x.Code == Session.User.Code);
+
+            string creditcardCencored = "";
+            for (int i = 0; i < member.Creditcard.Length; i++)
+            {
+                if (i < member.Creditcard.Length - 3)
+                {
+                    creditcardCencored += "*";
+                }
+                else
+                {
+                    creditcardCencored += member.Creditcard[i];
+                }
+            }
+
+            Console.Clear();
+            Console.WriteLine(
+                "Name:\t" + member.Name +
+                "\nEmail:\t" + member.Email +
+                "\nCode:\t" + member.Code +
+                "\nCreditcard:\t" + creditcardCencored);
+            ReadBackInput();
+        }
+        #endregion Membership Overview
     }
 }
