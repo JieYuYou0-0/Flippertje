ca02dbd9e66ce0fa0d7199ac2cb3a51bd561c6b4
Jie Yu You <jy_you@hotmail.com>
2022-07-06 04:31:36 +0200

    Update Members.cs

---
+++ jsonClasses/Members.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace GhibliFlix
+{
+    public class Members
+    {
+        public List<Member> members { get; set; }
+    }
+
+    public class Member : Guest
+    {
+        public string Code { get; set; }
+        public string Creditcard { get; set; }
+    }
+}
