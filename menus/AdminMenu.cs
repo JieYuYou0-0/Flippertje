00a886fef60959479a89a23a5222fcba7d58bb74
Melisa Çiftçi <1038404@hr.nl>
2022-07-06 09:29:47 +0200

    update AdminMenu.cs

--- menus/AdminMenu.cs
+++ menus/AdminMenu.cs
@@ -4,8 +4,9 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
+using Ghiblix.MovieMenu;
 
-namespace GhibliFlix
+namespace Ghiblix
 {
     class AdminMenu : Menu
     {
